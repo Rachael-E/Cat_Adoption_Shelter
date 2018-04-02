d96286c5d51408b18b334349fc6e1bcf39e42945
Rachael <rachael.ellen@btopenworld.com>
2018-04-02 15:08:52 +0100

    committing sql set_up, tables dropped and created

---
+++ db/cat_shelter.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS cats;
+DROP TABLE IF EXISTS owners;
+
+CREATE TABLE owners (
+  id SERIAL4 primary key,
+  name VARCHAR(255),
+  age INT4,
+  background TEXT,
+  accommodation_type VARCHAR(255),
+  total_existing_cats INT
+);
+
+CREATE TABLE cats (
+  id SERIAL4 primary key,
+  name VARCHAR(255),
+  age INT4,
+  gender VARCHAR(255),
+  description TEXT,
+  vaccinated VARCHAR(255),
+  admission_date VARCHAR(255),
+  status VARCHAR(255),
+  owner_id INT4 REFERENCES owners(id)
+);
