b0607faac001eb6b925136e38b72d67f124c46e3
Rachael <rachael.ellen@btopenworld.com>
2018-04-06 22:56:44 +0100

    tried changing app.rb route as heroku isn't functioning

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -7,10 +7,6 @@ class SqlRunner
       db = PG.connect( {dbname: 'd3c13snbupb3vj',
 host: 'ec2-54-83-19-244.compute-1.amazonaws.com',
 port: 5432, user: 'dfwvnnumngatag', password: '8eb9ae406e0d0de525dcc4c67c4e7ab478cde589d5a7195ba4e03c4dda7c76d2'})
-
-
-
-
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
